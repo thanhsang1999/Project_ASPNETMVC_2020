08886394dd9164def111830e963fca8576d843bc
thanpaint01 <55952922+thanpaint01@users.noreply.github.com>
2020-07-27 19:33:42 +0700

    hieu commit

---
+++ Project_ASPNETMVC_2020/Model/danhgia.cs
@@ -0,0 +1,34 @@
+namespace Project_ASPNETMVC_2020.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("project_2019.danhgia")]
+    public partial class danhgia
+    {
+        [Key]
+        [StringLength(255)]
+        public string ID_DANHGIA { get; set; }
+
+        [StringLength(255)]
+        public string ID_SANPHAM { get; set; }
+
+        [StringLength(255)]
+        public string ID_USER { get; set; }
+
+        [StringLength(255)]
+        public string SAO { get; set; }
+
+        [StringLength(255)]
+        public string TIEUDE { get; set; }
+
+        [StringLength(255)]
+        public string NOIDUNG { get; set; }
+
+        [StringLength(255)]
+        public string TEN_USER { get; set; }
+    }
+}
