5ba29380b2a4500a8ed2f13391a71da9c0d92a55
thanpaint01 <55952922+thanpaint01@users.noreply.github.com>
2020-08-01 16:31:53 +0700

    merge and create Folder

--- Project_ASPNETMVC_2020/Controllers/HomeController.cs
+++ Project_ASPNETMVC_2020/Controllers/HomeController.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using Project_ASPNETMVC_2020.Model.ModelOfSession;
+
 
 namespace Project_ASPNETMVC_2020.Controllers
 {
@@ -15,19 +18,34 @@ namespace Project_ASPNETMVC_2020.Controllers
             return View();
 
         }
-
-        public ActionResult About()
+        [HttpPost]
+        public JsonResult Login(UserLogin user)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            string UserName = user.UNAME;
+            string Password = user.PASS;
+            using (dbmodel)
+            {
+                string status;
+                var tmp = dbmodel.accounts.Count(a => a.USERNAME.Equals(user.UNAME) && a.PASSWORD.Equals(user.PASS));
+                if (tmp == 1)
+                {
+                    Session["UserName"] = UserName;
+                    status = "1";
+                }
+                else
+                {
+                    status = "0";
+                }
+                return new JsonResult { Data = new { status = status } };
+            }
+        }
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return RedirectToAction("Index", "Home");
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
     }
 }
