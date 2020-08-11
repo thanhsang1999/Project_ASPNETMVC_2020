f5466548c8617c7bea6f3589c2d49bc618e26ccd
thanpaint01 <55952922+thanpaint01@users.noreply.github.com>
2020-08-11 17:44:11 +0700

    checkout

---
+++ Project_ASPNETMVC_2020/Model/DAO/LikeDAO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_ASPNETMVC_2020.Model.DAO
+{
+    public class LikeDAO
+    {
+        public void addLike()
+        {
+
+        }
+    }
+}
