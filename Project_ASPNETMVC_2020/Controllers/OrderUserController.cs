ddd11154f7f955d86cde727d93ccd849659bfd2c
thanhsang1999 <nguyenhuythanhsang@gmail.com>
2020-08-28 23:27:44 +0700

    complete don hang user

---
+++ Project_ASPNETMVC_2020/Controllers/OrderUserController.cs
@@ -0,0 +1,34 @@
+﻿using Project_ASPNETMVC_2020.Model.EF;
+using Project_ASPNETMVC_2020.Model.ModelOfSession;
+using Project_ASPNETMVC_2020.Model.Order;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_ASPNETMVC_2020.Controllers
+{
+    public class OrderUserController : Controller
+    {
+        // GET: OrderUser
+        public ActionResult Index()
+        {
+            User user = Session["user"] as User;
+            if (user != null)
+            {
+                ArrayList model = new ArrayList();
+                var listOrder = OrderDAO.LoadOrderAccount(user.ID_ACCOUNT);
+                var listDetailOrder = OrderDAO.LoadDetailOrder(user.ID_ACCOUNT);
+                model.Add(listOrder);
+                model.Add(listDetailOrder);
+                return View(model);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+    }
+}
---
+++ Project_ASPNETMVC_2020/Model/Other/DetailOrderProduct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_ASPNETMVC_2020.Model.Other
+{
+    public class DetailOrderProduct
+    {
+        public string ID_ORDER { get; set; }
+        public string ID_PRODUCT { get; set; }
+        public decimal? AMOUNT { get; set; }
+        public string NAME { get; set; }
+    }
+}
