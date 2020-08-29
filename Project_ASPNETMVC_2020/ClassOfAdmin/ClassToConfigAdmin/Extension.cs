2fb06659ee6e54b8c8d6ea75e51fd7be754be66b
thanpaint01 <55952922+thanpaint01@users.noreply.github.com>
2020-08-30 00:51:51 +0700

    fail @using in Admin

---
+++ Project_ASPNETMVC_2020/ClassOfAdmin/ClassToConfigAdmin/Extension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using System.Web.UI;
+
+namespace Project_ASPNETMVC_2020.ClassOfAdmin.ClassToConfigAdmin
+{
+    public static class Extension
+    {
+        public static string ContentAssetAdmin(this UrlHelper urlHelper, string contentPath, bool toAbsolute = true)
+        {
+            contentPath = "~/Assets/admin/" + contentPath;
+            var path = urlHelper.Content(contentPath);
+            var url = new Uri(HttpContext.Current.Request.U
