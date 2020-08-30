e1d0203ad7e2fb008d13533091bbb7ad793a192b
thanpaint01 <55952922+thanpaint01@users.noreply.github.com>
2020-08-30 14:48:06 +0700

    fail import class

--- Project_ASPNETMVC_2020/ClassOfAdmin/ClassToConfigAdmin/Extension.cs
+++ Project_ASPNETMVC_2020/ClassOfAdmin/ClassToConfigAdmin/Extension.cs
@@ -12,8 +12,12 @@ namespace Project_ASPNETMVC_2020.ClassOfAdmin.ClassToConfigAdmin
 {
     public static class Extension
     {
-        public static string ContentAssetAdmin(this UrlHelper urlHelper, string contentPath, bool toAbsolute = true)
+        public static string ContentAssetWeb(this UrlHelper urlHelper, string contentPath, bool toAbsolute = true)
         {
             contentPath = "~/Assets/admin/" + contentPath;
             var path = urlHelper.Content(contentPath);
-            var url = new Uri(HttpContext.Current.Request.U
+            var url = new Uri(HttpContext.Current.Request.Url, path);
+            return toAbsolute ? url.AbsoluteUri : path;
+        }
+    }
+}
