ebc0850deb5736a6200dff027b1154910dae7b9c
thanhsang1999 <nguyenhuythanhsang@gmail.com>
2020-09-01 18:04:08 +0700

    commit 6h

--- Project_ASPNETMVC_2020/Areas/Admin/Controllers/SliderController.cs
+++ Project_ASPNETMVC_2020/Areas/Admin/Controllers/SliderController.cs
@@ -1,9 +1,11 @@
 ﻿using Project_ASPNETMVC_2020.Areas.Admin.Model.DAO;
 using Project_ASPNETMVC_2020.Areas.Admin.Model.Slider;
+using Project_ASPNETMVC_2020.Model.EF;
 using Project_ASPNETMVC_2020.Model.ModelOfSession;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -46,23 +48,97 @@ namespace Project_ASPNETMVC_2020.Areas.Admin.Controllers
             User user = Session["User"] as User;
             if (user != null && user.LEVEL.Equals("10"))
             {
-                var file = SLfile.FileName;
                 string status;
-                /*string Name = data.SLname;
-                var file = data.SLfile;
+                string Name = SLname;
+                var file = SLfile;
                 if (Name == null || file == null || Name == "")
-                {*/
-                if (file == null) { 
+                {
                     status = "empty";
                     return new JsonResult { Data = new { status = status } };
                 }
                 else
                 {
-                  /*  DBModel db1 = new DBModel();
-                    var TmpUser = db1.accounts.Where(x => x.ID_ACCOUNT.Equals(Idaccount)).FirstOrDefault();
-                    TmpUser.LEVEL = Level;
-                    TmpUser.ACTIVE = Active;
-                    db1.SaveChangesAsync();*/
+                    string image = System.IO.Path.GetFileName(file.FileName);
+                    string path = System.IO.Path.Combine(Server.MapPath("~/Assets/web/images/slider"), image);
+                    file.SaveAs(path);
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        file.InputStream.CopyTo(ms);
+                        byte[] array = ms.GetBuffer();
+                    }
+                    string idSlider = SliderDAO.GenerateIDSlider();
+                    int numberSlider = SliderDAO.GenerateNumberSlider();
+                    DBModel db = new DBModel();
+                    var tableSlider = db.sliders;
+                    slider tmpSlider = new slider();
+                    tmpSlider.ID_SLIDER = idSlider;
+                    tmpSlider.NAME = Name;
+                    tmpSlider.IMAGE = file.FileName;
+                    tmpSlider.NUMBER = numberSlider;
+                    tmpSlider.ACTIVE = 1;
+                    tableSlider.Add(tmpSlider);
+                    db.SaveChangesAsync();
+                    status = "success";
+                    return new JsonResult { Data = new { status = status } };
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home", new { area = "" });
+            }
+
+        }
+        public ActionResult EditSlider(string idSlider)
+        {
+            User user = Session["User"] as User;
+            if (user != null && user.LEVEL.Equals("10"))
+            {
+                slider tmpSlider = SliderDAO.LoadSlider(idSlider);
+                ArrayList model = new ArrayList();
+                model.Add(tmpSlider);
+                return View(model);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home", new { area = "" });
+            }
+        }
+        [HttpPost]
+        public ActionResult FormDataEditSlider(string SLname, HttpPostedFileBase SLfile,string SLnumber,string SLactive)
+        {
+            User user = Session["User"] as User;
+            if (user != null && user.LEVEL.Equals("10"))
+            {
+                string status;
+                string Name = SLname;
+                var file = SLfile;
+                if (Name == null || file == null || Name == "")
+                {
+                    status = "empty";
+                    return new JsonResult { Data = new { status = status } };
+                }
+                else
+                {
+                    string image = System.IO.Path.GetFileName(file.FileName);
+                    string path = System.IO.Path.Combine(Server.MapPath("~/Assets/web/images/slider"), image);
+                    file.SaveAs(path);
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        file.InputStream.CopyTo(ms);
+                        byte[] array = ms.GetBuffer();
+                    }
+                    string idSlider = SliderDAO.GenerateIDSlider();
+                    int numberSlider = SliderDAO.GenerateNumberSlider();
+                    DBModel db = new DBModel();
+                    var tableSlider = db.sliders;
+                    slider tmpSlider = new slider();
+                    tmpSlider.ID_SLIDER = idSlider;
+                    tmpSlider.NAME = Name;
+                    tmpSlider.IMAGE = file.FileName;
+                    tmpSlider.NUMBER = numberSlider;
+                    tmpSlider.ACTIVE = 1;
+                    tableSlider.Add(tmpSlider);
+                    db.SaveChangesAsync();
                     status = "success";
                     return new JsonResult { Data = new { status = status } };
                 }
--- Project_ASPNETMVC_2020/Areas/Admin/Model/DAO/SliderDAO.cs
+++ Project_ASPNETMVC_2020/Areas/Admin/Model/DAO/SliderDAO.cs
@@ -14,5 +14,43 @@ namespace Project_ASPNETMVC_2020.Areas.Admin.Model.DAO
             var result = db.sliders;
             return result.ToList();
         }
+        public static string GenerateIDSlider()
+        {
+            DBModel db = new DBModel();
+            var tmp = db.sliders;
+            int tmpInt = 0;
+            foreach (slider item in tmp)
+            {
+                int intCurrent = int.Parse(item.ID_SLIDER.Substring(2));
+                if (tmpInt <= intCurrent)
+                {
+                    tmpInt = intCurrent;
+                }
+
+            }
+            return "SL" + (tmpInt + 1);
+        }
+        public static int GenerateNumberSlider()
+        {
+            DBModel db = new DBModel();
+            var tmp = db.sliders;
+            int tmpInt = 0;
+            foreach (slider item in tmp)
+            {
+                int intCurrent = (int) item.NUMBER;
+                if (tmpInt <= intCurrent)
+                {
+                    tmpInt = intCurrent;
+                }
+
+            }
+            return (tmpInt + 1);
+        }
+        public static slider LoadSlider(string idSlider)
+        {
+            DBModel db = new DBModel();
+            var result = db.sliders.Where(x=>x.ID_SLIDER.Equals(idSlider)).FirstOrDefault();
+            return result;
+        }
     }
 }
